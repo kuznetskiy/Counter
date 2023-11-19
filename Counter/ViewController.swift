0a814d8b0b78577b5a3817a306be86e959a591aa
Anton Kuznetsov <kuznetskiy@gmail.com>
2023-11-19 21:51:26 +0300

    Добавление логики в приложение согласно заданию (Спринт 3: Базовая задача).

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func counterButtonTapped(_ sender: UIButton) {
+        counter += 1
+        counterLabel.text = "Значение счётчика: \(counter)"
+    }
 }
-
